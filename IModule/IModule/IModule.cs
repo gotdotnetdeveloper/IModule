b8163240ea6b54b7e25687daf25f3ca2770fa941
birulya <birulya@DENISBIRULYA>
2018-09-10 12:25:29 +0300

    Маркер Модуля. По аналогии с Prizm https://prismlibrary.github.io/docs/wpf/Modules.html

---
+++ IModule/IModule/IModule.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IModule
+{
+    /// <summary>
+    /// Маркер Модуля. По аналогии с Prizm https://prismlibrary.github.io/docs/wpf/Modules.html
+    /// </summary>
+    public interface IModule
+    {
+        /// <summary>
+        /// Инициализация. Регистрация нужных объектов в контейнере MvvmLight.Ioc.
+        /// </summary>
+        void Initialize();
+
+    }
+}
---
+++ IModule/IModule/StartInit.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace IModule
+{
+    /// <summary>
+    /// Регистрация сущьностей в контейнере при первом использовании. 
+    /// Одновременно может существовать несколько экземпляров контрола карты. Потокобезопастность обеспечивается "Двойной проверкой блокировки" https://en.wikipedia.org/wiki/Double-checked_locking#Usage_in_Microsoft_.NET_(Visual_Basic,_C#)
+    /// </summary>
+    public static class Module
+    {
+        private static readonly object _Lock = new object();
+        private static bool _ready = false;
+
+        /// <summary>
+        /// Проверка, что контрол работы с картами инициализировал необходимые классы в контейнере MvvmLight.Ioc.
+        /// </summary>
+        public static void EnsureInitialize()
+        {
+            if (!_ready)
+            {
+                lock (_Lock)
+                {
+                    if (!_ready)
+                    {
+                        InitializeModules();
+                        _ready = true;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Инициализация всех модулей (Наследников от IModule в текущей сборке)
+        /// </summary>
+        private static void InitializeModules()
+        {
+            var assm = Assembly.GetExecutingAssembly();
+            var moduleType = assm.GetTypes().Where(t => typeof(IModule).IsAssignableFrom(t) && t.IsClass && !t.IsAbstract);
+            foreach (var type in moduleType)
+            {
+                var instance = (IModule)Activator.CreateInstance(type);
+                instance.Initialize();
+            }
+        }
+    }
+}
